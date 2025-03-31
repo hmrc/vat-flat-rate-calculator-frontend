706c8ad19333a7ecf7d56646d9894f12fc1cb53a
Divya Prabha Manimaran <179695051+DivyaManimaran-hmrc@users.noreply.github.com>
2025-03-31 11:46:43 +0100

    DL-15874: Upgrade the dependencies version to the latest. (#118)
    Co-authored-by: DivyaManimaran_hmrc <179695051+DivyaManimaran_hmrc@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -20,9 +20,9 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.7.0"
-  val mongoVersion = "2.4.0"
-  val playFrontendVersion = "11.11.0"
+  val bootstrapVersion = "9.11.0"
+  val mongoVersion = "2.6.0"
+  val playFrontendVersion = "12.0.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
